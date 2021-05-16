2ffa92f634e5e555ff2c5c7b6df2f0c28c628294
harishking809 <harishking809@gmail.com>
2021-05-16 18:17:33 +0530

    removed dummy expenses from app.js file becoz in futher we use realdatabase to store and add expenses eg.firebase

--- src/app.js
+++ src/app.js
@@ -1,45 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom"; 
 import {Provider} from "react-redux";
-// import moment from "moment";
 import AppRouter from "./routers/AppRouter";
 import configStore from "./store/configStore";
-import {addExpense} from "./actions/expenses";
-import {setTextFilter} from "./actions/filters";
-import getVisibleExpanses from "./selectors/expenses";
 import 'react-dates/lib/css/_datepicker.css';  // the import belong to the src/componnet/ExpensesListFilters
 import "normalize.css/normalize.css"
 import "./styles/styles.scss";  
 
-const store=configStore();
-
- store.dispatch(addExpense({
-     description:"water bill",
-     note:"",
-     amount : 450,
-     createdAt:2023
- }));
-
- 
- store.dispatch(addExpense({
-    description:"Gas bill",
-    note:"",
-    amount : 1000,
-    createdAt:2021
-}));
-
-store.dispatch(addExpense({
-   description:"rent",
-   note:"",
-   amount : 109500,
-   createdAt:2022
-}));
- 
-
-const state=store.getState();
-const visibleExpenses=getVisibleExpanses(state.expenses, state.filters);
-
-// console.log(visibleExpenses);
+const store=configStore();  
 
 const jsx=(
     <div>
@@ -47,9 +15,5 @@ const jsx=(
            <AppRouter />
         </Provider>
     </div>
-)
-
-ReactDOM.render(jsx, document.getElementById("app"));
-
-// console.log(moment().startOf("month"));
-// console.log(moment().endOf("month"));
+) 
+ReactDOM.render(jsx, document.getElementById("app")); 
