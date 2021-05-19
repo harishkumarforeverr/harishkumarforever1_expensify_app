b295e490cb51ecab41b920ed03f37a14bb3e89bc
harishking809 <harishking809@gmail.com>
2021-05-19 11:51:08 +0530

    setuped the two firebase database , one for prdoction/development and another for testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import "firebase/database";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCQ6D7IUwC7WjuyJfjIniqN1liMTn0OUiA",
-    authDomain: "expensify-app-cc6d9.firebaseapp.com",
-    databaseURL: "https://expensify-app-cc6d9-default-rtdb.firebaseio.com",
-    projectId: "expensify-app-cc6d9",
-    storageBucket: "expensify-app-cc6d9.appspot.com",
-    messagingSenderId: "672532568508",
-    appId: "1:672532568508:web:9326499991da09023d33b9",
-    measurementId: "G-NGVDXWW4M0"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain:process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATA_BASE_URL,
+    projectId: process.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
 firebase.initializeApp(firebaseConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
-const path = require("path");
-
+const path = require("path"); 
+const dotEnv=require("dotenv");
+const webpack=require("webpack");
 module.exports=(env)=>{  
-  // console message
+  // console message 
+  if(process.env.NODE_ENV==='test'){ 
+        console.log("your using test database of firebase ");
+        dotEnv.config({
+          path:".env.test"
+        })
+  }else if(process.env.NODE_ENV==='development'){   
+      console.log("your using developmemnt/production firebase ");
+      dotEnv.config({
+        path:".env.development"
+      }) 
+  }
+  console.log("process.env.NODE_ENV",process.env.NODE_ENV);
   console.log(env.production?"your running your build in Procution Mode":"your running your build in Development Mode");
   // variable declartion
   const envMode=env.production?"production":"development";
@@ -44,6 +57,16 @@ module.exports=(env)=>{
     plugins : [
       new miniCssExtractPlugin({
         filename:"styles.css"
+      }),
+      new webpack.DefinePlugin({  
+        "process.env.FIREBASE_API_KEY" : JSON.stringify(process.env.FIREBASE_API_KEY),
+        "process.env.FIREBASE_AUTH_DOMAIN" : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+         "process.env.FIREBASE_DATA_BASE_URL" : JSON.stringify(process.env.FIREBASE_DATA_BASE_URL),
+         "process.FIREBASE_PROJECT_ID" : JSON.stringify(process.FIREBASE_PROJECT_ID),
+         "process.env.FIREBASE_STORAGE_BUCKET" : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+         "process.env.FIREBASE_MESSAGING_SENDER_ID" : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+         "process.env.FIREBASE_APP_ID" : JSON.stringify(process.env.FIREBASE_APP_ID),
+         "process.env.FIREBASE_MEASUREMENT_ID" : JSON.stringify(process.env.FIREBASE_MEASUREMENT_I)     
       })
     ],
     devtool:env.production?"source-map":"eval-cheap-module-source-map",
@@ -51,8 +74,8 @@ module.exports=(env)=>{
       contentBase:path.join(__dirname,'public'),
       port:9090,
       historyApiFallback:true,
-      publicPath:"/dist/"
+      publicPath:"/dist/", 
     },
-    mode:envMode
-}
+    mode:envMode 
+ }
 }
